a75bbf34008ac3a360dec50b8ab6b01f9cc204f2
Kyle <kmstumpff@gmail.com>
2014-06-26 14:38:33 -0400

    Need to create TestTrack dir

--- Unix/setupUnix.sh
+++ Unix/setupUnix.sh
@@ -533,6 +533,7 @@ else
 		filename="ttlinuxinstall_$build.tar.gz"
 		echo ""
 		echo "Downloading TestTrack $nRelease $build"
+		mkdir /home/seapine/Desktop/TestTrack >> /dev/null
 		mkdir /home/seapine/Desktop/TestTrack/$build >> /dev/null
 		if [[ ("$buildnum" = "$lastBuild") ]]
 		then
