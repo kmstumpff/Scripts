33424f642db66ad2285236e108d2f6ea7ed9fa59
Seapine <seapine@debian>
2014-07-30 10:54:11 -0400

    moved setupUnix.sh to Unix dir

--- Unix/setup/yum_x86-64_libs.sh
+++
@@ -1,2 +0,0 @@
-sudo yum install glibc.i686 zlib.i686 libstdc++.i686 glib2.i686 libICE.i686 libSM.i686 libXext.i686 freetype.i686 libXrender.i686 fontconfig.i686
-ldd scmserver
--- Unix/testing/sudo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo -v
-while true; do sudo -n true; sleep 60; done 2>/dev/null &
